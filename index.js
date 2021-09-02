4ccbbbcaf93b64d0a6b537004a7ff6a7ad484bc0
sh-kik <tori.dehanai@gmail.com>
2021-09-02 23:06:04 +0900

    add  eventLisner

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // comment
-console.log('Hello');
+const form = document.getElementById("form")
+const input = document.getElementById("input")
+
+form.addEventListener("submit", function(event){
+    event.preventDefault()
+    console.log("input.value")
+})
